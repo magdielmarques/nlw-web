95615f397ce4dbe61e3e758827b064517215aa26
magdielmarques <contato.magdiel@gmail.com>
2021-07-29 10:11:14 -0400

    Construção HTML primeira página v3 50 min

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
 import React from 'react';
-import { useState } from 'react';
 import './App.css';
-
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title={`Contador: ${counter}`}/>
-
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
-
-      <h1>{counter * 2}</h1>
-
-    </div>
-    );
+    <Home />  
+  );
 }
 
 export default App;
