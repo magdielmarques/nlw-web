ab09112f71a6919b16fc2d2c5824105ded248d76
magdielmarques <contato.magdiel@gmail.com>
2021-09-15 14:10:05 -0400

    Passando variável de ambiente

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 import Routes from './routes';
 
 function App() {
+  fetch(process.env.REACT_APP_API_URL || 'http://localhost:3333')
+    .then(() => console.log('DEU CERTO'))
+    .catch(() => console.log('DEU ERRO'))
   return (
     <Routes />
   );
